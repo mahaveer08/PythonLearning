41594f27fc6cee108add86f4abf32eadc07d4016
mahaveer08 <mahaveer.db@cosmitude.com>
2023-02-03 21:08:30 +0530

    New commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<html><body><h1>Welcome to Devops!</h1>\n<p><h3>Lets conquer it at the hardest!</h3></p></body></html>'
+
+if __name__ == '__main__':
+    app.run()
+
