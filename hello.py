b177065e814c99897c22e30c04a59415ab422b2a
mahaveer08 <mahaveer.db@cosmitude.com>
2023-02-05 09:43:51 +0530

    hello.py file updated

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print('Successfully running python code')
     return '<html><body><h1>Welcome to Devops!</h1>\n<p><h3>Lets conquer it at the hardest!</h3></p></body></html>'
 
 if __name__ == '__main__':
